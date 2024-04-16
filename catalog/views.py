232db20c8ee99220128cc75a34612bf4955f535e
HungerforCodes <50112089+HungerforCodes@users.noreply.github.com>
2024-04-16 22:27:58 +0800

    added add/delete/update author but with error

--- catalog/views.py
+++ catalog/views.py
@@ -8,6 +8,10 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from catalog.forms import RenewBookForm
 
@@ -70,14 +74,40 @@ def renew_book_librarian(request, pk):
         if form.is_valid():
             book_instance.due_back = form.cleaned_data['renewal_date']
             book_instance.save()
-            return HttpResponseRedirect(reverse('all-borrowed'))
-        else:
-            proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-            form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+            return HttpResponseRedirect(reverse('index'))
+    
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
         
-        context = {
-            'form': form,
-            'book_instance': book_instance,
-        }
-        
-        return render(request, 'catalog/book_renew_librarian.html', context)
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+    
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/11/2023'}
+    permission_required = 'catalog.add_author'
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    fields = '__all__'
+    permission_required = 'catalog.change_author'
+    
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
+    
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("author-delete", kwargs={"pk": self.object.pk})
+            )
